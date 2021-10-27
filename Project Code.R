97ccf2f8564dd1ea5879f3f79774169d2be21bd0
Connor Fuglestad <cfuglestad@wisc.edu>
2021-10-26 21:27:55 -0500

    Update Project Code.R

--- Project Code.R
+++ Project Code.R
@@ -24,15 +24,23 @@ setwd("/Users/fuglc/725 Work/Project")
 # data we downloaded at the beginning of the semester. deal closed near the 
 # end of q4 2014, so take  data from 2013 as pre period and 2014 as post.
 
-#read in all data
-data_2013_q1 <- read_csv("data/2013_q1.csv")
-data_2013_q2 <- read_csv("data/2013_q2.csv")
-data_2013_q3 <- read_csv("data/2013_q3.csv")
-data_2013_q4 <- read_csv("data/2013_q4.csv")
-data_2014_q1 <- read_csv("data/2014_q1.csv")
-data_2014_q2 <- read_csv("data/2014_q2.csv")
-data_2014_q3 <- read_csv("data/2014_q3.csv")
-data_2014_q4 <- read_csv("data/2014_q4.csv")
+#read in all data subsetted for American and US Airways
+data_2013_q1 <- read_csv("data/2013_q1.csv") %>% 
+  subset(TICKET_CARRIER=="AA" | TICKET_CARRIER=="US")
+data_2013_q2 <- read_csv("data/2013_q2.csv") %>% 
+  subset(TICKET_CARRIER=="AA" | TICKET_CARRIER=="US")
+data_2013_q3 <- read_csv("data/2013_q3.csv") %>% 
+  subset(TICKET_CARRIER=="AA" | TICKET_CARRIER=="US")
+data_2013_q4 <- read_csv("data/2013_q4.csv") %>% 
+  subset(TICKET_CARRIER=="AA" | TICKET_CARRIER=="US")
+data_2014_q1 <- read_csv("data/2014_q1.csv") %>% 
+  subset(TICKET_CARRIER=="AA" | TICKET_CARRIER=="US")
+data_2014_q2 <- read_csv("data/2014_q2.csv") %>% 
+  subset(TICKET_CARRIER=="AA" | TICKET_CARRIER=="US")
+data_2014_q3 <- read_csv("data/2014_q3.csv") %>% 
+  subset(TICKET_CARRIER=="AA" | TICKET_CARRIER=="US")
+data_2014_q4 <- read_csv("data/2014_q4.csv") %>% 
+  subset(TICKET_CARRIER=="AA" | TICKET_CARRIER=="US")
 
 data_pre <- rbind.fill(data_2013_q1,
                        data_2013_q2,
@@ -43,20 +51,49 @@ data_post <- rbind.fill(data_2014_q1,
                         data_2014_q3,
                         data_2014_q4)
 
-
-
-
-
-
-
-
-
-
-
-#set all data - omt 2000, as it was not a full data set
-data_full <- rbind.fill(data_2001_q1_q4, data_2002_q1_q4, data_2003_q1_q4, data_2004_q1_q4, data_2005_q1_q4, data_2006_q1_q4, data_2007_q1_q4, data_2008_q1_q4, data_2009_q1_q4, data_2010_q1_q4, data_2011_q1_q4, data_2012_q1_q4, data_2013_q1_q4, data_2014_q1_q4, data_2015_q1_q4, data_2016_q1_q4, data_2017_q1_q4, data_2018_q1_q4, data_2019_q1_q4, data_2020_q1_q4)
-
 # b. Clean this up so we get the variables we have used in our problem sets
+
+# Start by removing any observations with a ticket carrier change
+data_pre1 <- subset(data_pre, data_pre$TK_CARRIER_CHANGE==0)
+data_post1 <- subset(data_post, data_post$TK_CARRIER_CHANGE==0)
+
+# remove outlier prices 
+data_pre2 <- subset(data_pre1, data_pre1$MARKET_FARE >= 25.0 & data_pre1$MARKET_FARE<=2500.0)
+data_post2 <- subset(data_post1, data_post1$MARKET_FARE >= 25.0 & data_post1$MARKET_FARE<=2500.0)
+
+# concatenate market IDs to get a market indicator
+data_pre3 <- data_pre2 %>% 
+  mutate(Market_Ind = paste(data_pre2$ORIGIN_AIRPORT_ID,data_pre2$DEST_AIRPORT_ID,sep=""))
+data_post3 <- data_post2 %>% 
+  mutate(Market_Ind = paste(data_post2$ORIGIN_AIRPORT_ID,data_post2$DEST_AIRPORT_ID,sep=""))
+
+# Join the list of markets with averages over 20 passengers per day back to 
+# the ticket level data using an inner join to keep only the ticket level 
+# observations where the market averages over 20 passengers a day
+data_pre4 <- data_pre3 %>% 
+  dplyr::group_by(Market_Ind) %>% 
+  dplyr::summarize(Tot_Passengers=sum(PASSENGERS)) %>% 
+  mutate(daily_ave=Tot_Passengers*(4/365)) %>% 
+  dplyr::filter(daily_ave > 20)
+data_pre5 <- inner_join(data_pre3,data_pre4,by="Market_Ind")
+data_post4 <- data_post3 %>% 
+  dplyr::group_by(Market_Ind) %>% 
+  dplyr::summarize(Tot_Passengers=sum(PASSENGERS)) %>% 
+  mutate(daily_ave=Tot_Passengers*(4/365)) %>% 
+  dplyr::filter(daily_ave > 20)
+data_post5 <- inner_join(data_post3,data_post4,by="Market_Ind")
+
+# Get data onto a market airline level by grouping by market indicator and carrier
+Market_airline_pre <- data_pre5 %>% 
+  dplyr::group_by(Market_Ind, TICKET_CARRIER) %>% 
+  dplyr::summarize(ave_price=mean(MARKET_FARE, na.rm=T),
+            Tot_Pass_by_market_airline=sum(PASSENGERS),
+            ave_distance=mean(MARKET_DISTANCE, na.rm=T))
+Market_airline_post <- data_post5 %>% 
+  dplyr::group_by(Market_Ind, TICKET_CARRIER) %>% 
+  dplyr::summarize(ave_price=mean(MARKET_FARE, na.rm=T),
+                   Tot_Pass_by_market_airline=sum(PASSENGERS),
+                   ave_distance=mean(MARKET_DISTANCE, na.rm=T))
 
 # c. Split into a test set (pre period), validation set (pre period), and a test
 # set (post period)
